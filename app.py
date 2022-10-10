402bfd8e6c94dc982bb3c1d458bf0d1c482402ba
Shubham Singla <shubhamsingla369@gmail.com>
2022-10-11 02:59:06 +0530

    redirect success/cancelled

--- app.py
+++ app.py
@@ -269,15 +269,6 @@ def create_checkout_session():
     stripe.api_key = stripe_keys["secret_key"]
 
     try:
-        # Create new Checkout Session for the order
-        # Other optional params include:
-        # [billing_address_collection] - to display billing address details on the page
-        # [customer] - if you have an existing Stripe Customer ID
-        # [payment_intent_data] - capture the payment later
-        # [customer_email] - prefill the email input in the form
-        # For full details see https://stripe.com/docs/api/checkout/sessions/create
-
-        # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
         checkout_session = stripe.checkout.Session.create(
             success_url=domain_url + "success?session_id={CHECKOUT_SESSION_ID}",
             cancel_url=domain_url + "cancelled",
@@ -298,6 +289,16 @@ def create_checkout_session():
         return jsonify({"sessionId": checkout_session["id"]})
     except Exception as e:
         return jsonify(error=str(e)), 403
+        
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+
+@app.route("/cancelled")
+def cancelled():
+    return render_template("cancelled.html")
 
 
 if __name__ == '__main__':
